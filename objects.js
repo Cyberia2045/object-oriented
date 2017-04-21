1e4344f8c5a72c81e77d9459a64d03ccac4311d9
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-21 16:04:41 -0400

    created person, student, and teacher object constructors closes #2

--- objects.js
+++ objects.js
@@ -18,32 +18,71 @@
 
 // Exercise Two
 
-function Photo(fileName, category) {
-	this.fileName = fileName;
-	this.category = category;
+// function Photo(fileName, category) {
+// 	this.fileName = fileName;
+// 	this.category = category;
+// }
+
+// function Album(albumName) {
+// 	this.album = [];
+// 	this.albumName = albumName;
+// 	this.addPhoto = addPhoto;
+// }
+
+// function addPhoto(photo) {
+// 	this.album.push(photo);
+// } 
+
+// function albumList(philly) {
+// 	console.log(philly);
+// 	for (var i=0; i < philly.length; i++) {
+// 		console.log("Photo " + i + ": " + philly[i].category);
+// 	}
+// }
+
+// var colorado = new Photo("colorado.jpg", "hiking");
+// var wyoming = new Photo("tetons.jpg", "hiking");
+// var vermont = new Photo("jay-peak.jpg", "hiking";)
+// var hikingPhotos = new Album("hikingPhotos");
+
+// Exercise Three
+
+function Person(fName, lName, neighborhood, age) {
+	this.fName = fName;
+	this.lName = lName;
+	this.neighborhood = neighborhood;
+	this.age = age;
 }
 
-function Album(albumName) {
-	this.album = [];
-	this.albumName = albumName;
-	this.addPhoto = addPhoto;
+function Student(fName, lName, neighborhood, age, university, subject) {
+	Person.apply(this, arguments);
+	this.university = university;
+	this.subject = subject;
 }
 
-function addPhoto(photo) {
-	this.album.push(photo);
-} 
-
-function albumList(philly) {
-	console.log(philly);
-	for (var i=0; i < philly.length; i++) {
-		console.log("Photo " + i + ": " + philly[i].category);
-	}
+function Teacher() {
+	Student.apply(this, arguments);
 }
 
-var colorado = new Photo("colorado.jpg", "hiking");
-var wyoming = new Photo("tetons.jpg", "hiking");
-var vermont = new Photo("jay-peak.jpg", "hiking")
-var hikingPhotos = new Album("hikingPhotos");
+var sam = new Student("Sam", "Almeida", "Wilmington", "32", "Wagner", "Poli Sci");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
