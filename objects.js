276b5a4a7a6883d7618a34e55a0c3de7450f33c0
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-24 09:44:25 -0400

    added methods to objects

--- objects.js
+++ objects.js
@@ -47,28 +47,66 @@
 
 // Exercise Three
 
-function Person(fName, lName, neighborhood, age) {
+function Person(fName, lName, location, age) {
 	this.fName = fName;
 	this.lName = lName;
-	this.neighborhood = neighborhood;
+	this.location = location;
 	this.age = age;
+}
+
+function University(name, location, tuition) {
+	this.name = name;
+	this.location = location;
+	this.tuition = tuition;
 }
 
 function Student(fName, lName, neighborhood, age, university, subject) {
 	Person.apply(this, arguments);
 	this.university = university;
 	this.subject = subject;
+	this.inState = inState;
 }
 
 function Teacher() {
 	Student.apply(this, arguments);
 }
 
-var sam = new Student("Sam", "Almeida", "Wilmington", "32", "Wagner", "Poli Sci");
+// students
+var sam = new Student("Sam", "Almeida", "Delaware", "32", "Wagner", "Poli Sci");
+var mateo = new Student("Matthew", "MacDermant", "Pennsylvania", "30", "Wagner", "History");
+var amanda = new Student("Amanda", "Michael", "Pennsylvania", "30", "Villanova", "Psychology");
 
+// teachers
+var phillip = new Teacher("Phillip", "Wentworth", "Pennsylvania", "35", "Penn", "Classics");
+var sameer = new Teacher("Sameer", "Saxena", "Pennsylvania", "27", "Penn", "Botony");
 
+// universities
+var wagner = new University("Wagner", "New York", 40000);
+var villanova = new University("Villanova", "Pennsylvania", 35000);
+var penn = new University("Penn", "Pennsylvania", 40000);
 
+function studentList(studentArray) {
+	for (var i=0; i < studentArray.length; i++) {
+		console.log(studentArray[i].fName + " " + studentArray[i].lName + " is a " + studentArray[i].age + " year old student of " + studentArray[i].subject + " at " + studentArray[i].university + ".")
+	}
+}
 
+function teacherList(teacherArray) {
+	for(var i=0; i < teacherArray.length; i++) {
+		console.log(teacherArray[i].fName + " " + teacherArray[i].lName + " is a professor of " + teacherArray[i].subject + " at " + teacherArray[i].university + ".")
+	}
+}
+
+// function inState() {
+// 	for(var i=0; i < studentArray.length)
+// 	if (this.student.location === university.location) {
+// 		this.student.tuition = (this.student.tuition)/2
+// 	}
+// 	else {
+// 		console.log("Full Price")
+// 	}
+// 		console.log(this.student.tuition)
+// }
 
 
 
