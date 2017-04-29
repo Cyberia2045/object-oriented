045ae5b0b37717e4d9aa5ba72b2793d4533c54b0
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-29 00:50:13 -0400

    created object methods for photo albums

--- objects.js
+++ objects.js
@@ -18,94 +18,98 @@
 
 // Exercise Two
 
-// function Photo(fileName, category) {
-// 	this.fileName = fileName;
-// 	this.category = category;
-// }
+function Photo(fileName, category) {
+	this.fileName = fileName;
+	this.category = category;
+}
 
-// function Album(albumName) {
-// 	this.album = [];
-// 	this.albumName = albumName;
-// 	this.addPhoto = addPhoto;
-// }
+function Album(albumName) {
+	this.album = [];
+	this.albumName = albumName;
+	this.addPhoto = addPhoto;
+	this.albumList = albumList;
+}
 
-// function addPhoto(photo) {
-// 	this.album.push(photo);
-// } 
+function addPhoto(photo) {
+	this.album.push(photo);
+} 
 
-// function albumList(philly) {
-// 	console.log(philly);
-// 	for (var i=0; i < philly.length; i++) {
-// 		console.log("Photo " + i + ": " + philly[i].category);
-// 	}
-// }
+function albumList() {
+	for (var i=0; i < this.album.length; i++) {
+		console.log("Photo " + i + ": " + this.album[i].fileName);
+	}
+}
 
-// var colorado = new Photo("colorado.jpg", "hiking");
-// var wyoming = new Photo("tetons.jpg", "hiking");
-// var vermont = new Photo("jay-peak.jpg", "hiking";)
-// var hikingPhotos = new Album("hikingPhotos");
+var colorado = new Photo("colorado.jpg", "hiking");
+var wyoming = new Photo("tetons.jpg", "hiking");
+var vermont = new Photo("jayPeak.jpg", "hiking");
+var hikingPhotos = new Album("hikingPhotos");
+
+hikingPhotos.addPhoto(colorado);
+hikingPhotos.addPhoto(wyoming);
+hikingPhotos.addPhoto(vermont);
 
 // Exercise Three
 
-function Person(fName, lName, location, age) {
-	this.fName = fName;
-	this.lName = lName;
-	this.location = location;
-	this.age = age;
-}
+// function Person(fName, lName, location, age) {
+// 	this.fName = fName;
+// 	this.lName = lName;
+// 	this.location = location;
+// 	this.age = age;
+// }
 
-function University(name) {
-	this.name = name;
-	this.students = [];
-	this.teachers = [];
-	this.addStudent = addStudent;
-	this.addTeacher = addTeacher;
-	this.studentList = studentList;
-	this.teacherList = teacherList;
-}
+// function University(name) {
+// 	this.name = name;
+// 	this.students = [];
+// 	this.teachers = [];
+// 	this.addStudent = addStudent;
+// 	this.addTeacher = addTeacher;
+// 	this.studentList = studentList;
+// 	this.teacherList = teacherList;
+// }
 
-function Student(fName, lName, neighborhood, age, university, subject) {
-	Person.apply(this, arguments);
-	this.university = university;
-	this.subject = subject;
-}
+// function Student(fName, lName, neighborhood, age, university, subject) {
+// 	Person.apply(this, arguments);
+// 	this.university = university;
+// 	this.subject = subject;
+// }
 
-function Teacher() {
-	Student.apply(this, arguments);
-}
+// function Teacher() {
+// 	Student.apply(this, arguments);
+// }
 
-// students
-var sam = new Student("Sam", "Almeida", "Delaware", "32", "Wagner", "Poli Sci");
-var mateo = new Student("Matthew", "MacDermant", "Pennsylvania", "30", "Wagner", "History");
-var amanda = new Student("Amanda", "Michael", "Pennsylvania", "30", "Villanova", "Psychology");
+// // students
+// var sam = new Student("Sam", "Almeida", "Delaware", "32", "Wagner", "Poli Sci");
+// var mateo = new Student("Matthew", "MacDermant", "Pennsylvania", "30", "Wagner", "History");
+// var amanda = new Student("Amanda", "Michael", "Pennsylvania", "30", "Villanova", "Psychology");
 
-// teachers
-var phillip = new Teacher("Phillip", "Wentworth", "Pennsylvania", "35", "Wagner", "Classics");
-var sameer = new Teacher("Sameer", "Saxena", "Pennsylvania", "27", "Villanova", "Botony");
+// // teachers
+// var phillip = new Teacher("Phillip", "Wentworth", "Pennsylvania", "35", "Wagner", "Classics");
+// var sameer = new Teacher("Sameer", "Saxena", "Pennsylvania", "27", "Villanova", "Botony");
 
-// universities
-var wagner = new University("Wagner");
-var villanova = new University("Villanova");
+// // universities
+// var wagner = new University("Wagner");
+// var villanova = new University("Villanova");
 
-function studentList() {
-	for (var i=0; i < this.students.length; i++) {
-		console.log(this.students[i].fName + " " + this.students[i].lName)
-	}
-}
+// function studentList() {
+// 	for (var i=0; i < this.students.length; i++) {
+// 		console.log(this.students[i].fName + " " + this.students[i].lName)
+// 	}
+// }
 
-function teacherList() {
-	for(var i=0; i < this.teachers.length; i++) {
-		console.log(this.teachers[i].fName + " " + this.teachers[i].lName + " is a professor of " + this.teachers[i].subject + " at " + this.teachers[i].university + ".")
-	}
-}
+// function teacherList() {
+// 	for(var i=0; i < this.teachers.length; i++) {
+// 		console.log(this.teachers[i].fName + " " + this.teachers[i].lName + " is a professor of " + this.teachers[i].subject + " at " + this.teachers[i].university + ".")
+// 	}
+// }
 
-function addStudent(student) {
-	this.students.push(student);
-}
+// function addStudent(student) {
+// 	this.students.push(student);
+// }
 
-function addTeacher(teacher) {
-	this.teachers.push(teacher);
-}
+// function addTeacher(teacher) {
+// 	this.teachers.push(teacher);
+// }
 
 
 
